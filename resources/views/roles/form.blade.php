ebf53ff2bd84e32e6f889c37e42f80722c6b9a87
= <stavros.mastorakis@outlook.com>
2024-09-26 22:43:10 +0300

    added greek translation for roles

--- resources/views/roles/form.blade.php
+++ resources/views/roles/form.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
 <div class="container">
-    <h1>{{ isset($role) ? 'Edit Role' : 'Create Role' }}</h1>
+    <h1>{{ isset($role) ? __('Edit Role') : __('Create Role') }}</h1>
     <form action="{{ isset($role) ? route('roles.update', $role) : route('roles.store') }}" method="POST">
         @csrf
         @if(isset($role))
             @method('PUT')
         @endif
         <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
+            <label for="name" class="form-label">{{ __('Name') }}</label>
             <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $role->name ?? '') }}" required>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     </form>
 </div>
 @endsection
--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 <div class="container">
-    <h1>Roles</h1>
-    <a href="{{ route('roles.create') }}" class="btn btn-primary mb-3">Create New Role</a>
+    <h1>{{ __('Roles') }}</h1>
+    <a href="{{ route('roles.create') }}" class="btn btn-primary mb-3">{{ __('Create New Role') }}</a>
     <div class="table-responsive">
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>ID</th>
-                    <th>Name</th>
-                    <th>Actions</th>
+                    <th>{{ __('Name') }}</th>
+                    <th>{{ __('Actions') }}</th>
                 </tr>
             </thead>
             <tbody>
@@ -20,11 +20,11 @@
                     <td>{{ $role->name }}</td>
                     <td>
                         <div class="btn-group" role="group">
-                            <a href="{{ route('roles.edit', $role) }}" class="btn btn-sm btn-primary">Edit</a>
+                            <a href="{{ route('roles.edit', $role) }}" class="btn btn-sm btn-primary">{{ __('Edit') }}</a>
                             <form action="{{ route('roles.destroy', $role) }}" method="POST" class="d-inline">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('{{ __('Are you sure?') }}')">{{ __('Delete') }}</button>
                             </form>
                         </div>
                     </td>
